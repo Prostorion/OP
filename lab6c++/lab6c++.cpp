35ff083216bc9b658dfbfafc8b0798f76761b790
Prostorion <50420552+Prostorion@users.noreply.github.com>
2021-12-25 17:58:30 +0200

    Лабораторна 6 варіант 25 с++

---
+++ lab6c++/lab6c++.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double myAtan(double x, int n);
+void result(double x, int n);
+
+int main() {
+	double x;
+	int n;
+	cout << "Enter number: ";
+	cin >> x;
+	cout << "Enter accuracy (1/number): ";
+	cin >> n;
+	cout << "arctg: " << myAtan(x, n) << endl;
+	result(x, n);
+
+}
+
+double myAtan(double x, int n) {
+	double a = x;
+	double sum = a;
+	double b = a;
+	double E = 1 / n;
+	for (int i = 1; a > E; i++) {
+		b *= -x * x;
+		a *= b / (2 * i + 1);
+		sum += a;
+	}
+	return sum;
+}
+
+void result(double x, int n) {
+	cout << "Result: ";
+	if (x > 1) {
+		cout << myAtan(x, n) / myAtan(x - 5, n);
+	}
+	else if (x <= 1 && x >= 0) {
+		cout << myAtan(x, n) + myAtan(2 * x, n);
+	}
+	else cout << "x isn't match";
+}
